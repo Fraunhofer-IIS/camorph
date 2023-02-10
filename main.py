7584c79d6e4bfcb3b2001db1b7c232078bbe7b58
Michel Bätz <michel.baetz@iis.fraunhofer.de>
2023-02-10 15:44:51 +0100

    Cleaned up main.py and added posetrace example

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-
 import camorph.camorph as camorph
 
-
+# Example for converting from COLMAP to FBX (with visualization of the camera poses)
 cams = camorph.read_cameras('COLMAP',r'\\path\to\colmap')
 
 camorph.visualize(cams)
@@ -9,21 +8,12 @@ camorph.visualize(cams)
 camorph.write_cameras('fbx', r'\\path\to\file.fbx', cams)
 
 
+# Example for converting MPEG-I OMAF pose trace to NeRF JSON
+inputFile = r'\\path\to\input.json'
+addInputFile = r'\\path\to\posetrace.csv'
+outputFile = r'\\path\to\output.json'
 
-cams = camorph.read_cameras('unity', r"\\netapp01\bt\Orga\Mitarbeiter\brandbn\BA\02_Research\Formats\Unity\LFTORv2_capture_Lighthouse01.unity")
+cams = camorph.read_cameras('mpeg_omaf', inputFile, addInputFile, posetrace="posetrace")
 
-camorph.visualize(cams)
-
-#camorph.write_cameras('fbx', r"\\path\to\file.fbx", cams)
-
-
-
-
-
-
-# Profiling
-#cProfile.run('camorph.read_cameras("unity", r"\\\\netapp01\\bt\\Orga\\Mitarbeiter\\brandbn\\BA\\02_Research\\Formats\\time_complexity\\unity_300.unity")', 'stats')
-#cProfile.run('camorph.read_cameras("fbx", r"\\\\netapp01\\bt\\Orga\\Mitarbeiter\\brandbn\\BA\\02_Research\\Formats\\fbx\\BlenderTestScene_texture_animation.fbx")', 'stats')
-#p = pstats.Stats('stats')
-#p.sort_stats('cumtime').print_stats()
+camorph.write_cameras("nerf", outputFile, cams)
 
